cd9a7b8b7e050adeddc7de9c42ad0e51dbc6003d
atian722 <ayt2@rice.edu>
2025-03-05 13:39:01 -0600

    initializing variables

--- BPM_IoT/BPM_IoT.ino
+++ BPM_IoT/BPM_IoT.ino
@@ -1,3 +1,17 @@
+#include "thingProperties.h"
+int sensor_pin = 0;
+int pulse_signal = 0;
+int upper_threshold = 860; //or your threshold of choice
+int lower_threshold = 800; //or your threshold of choice
+int counter = 0;
+
+//float BPM = 0.0;
+bool ignore = false;
+bool first_pulse_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+unsigned long pulse_period = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
